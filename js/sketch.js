b699dae9137826dc318d7e84c0c027eaf7a2eb09
Ross Goodwin <ross.goodwin@gmail.com>
2016-02-09 13:28:57 -0500

    whoops

--- js/sketch.js
+++ js/sketch.js
@@ -46,7 +46,7 @@ function draw() {
         ambientMaterial(10);
       }
       var bh = blackholes[i];
-      var radius = bh['radius_ratio'] * sphereSize / frameCount;
+      var radius = bh['radius_ratio'] * sphereSize;
       ellipsoid(radius,radius,radius);
 
       if (i < blackholes.length-stepSize) {
