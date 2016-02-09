f300e9b940951ffdbcd47411bc9297e12d8bcdad
Ross Goodwin <ross.goodwin@gmail.com>
2016-02-09 13:40:08 -0500

    changed material

--- js/sketch.js
+++ js/sketch.js
@@ -43,7 +43,7 @@ function draw() {
         basicMaterial(249, 105, 14);
       } 
       else {
-        ambientMaterial(2,0,3,235);
+        ambientMaterial(10,8,12,235);
       }
       var bh = blackholes[i];
       var radius = bh['radius_ratio'] * sphereSize;
